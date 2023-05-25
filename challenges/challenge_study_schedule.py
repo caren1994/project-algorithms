8dc0f66afcadc3f327594d99bab9558456757ee4
caren <carenoliveirasilvapontes@gmail.com>
2023-05-25 14:30:14 -0300

    feat:challenge concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        count = 0
+        for entrada, saida in permanence_period:
+            if entrada <= target_time and saida >= target_time:
+                count += 1
+        return count
+    except TypeError:
+        return None

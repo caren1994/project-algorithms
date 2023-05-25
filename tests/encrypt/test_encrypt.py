a47b39fdc2ccd4949f9160145c2eccf44b26d375
caren <carenoliveirasilvapontes@gmail.com>
2023-05-25 15:09:03 -0300

    feat:concluido challenge test

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,10 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(1, "paz")
+
+    assert encrypt_message("paz", 2) == ("z_ap")
+    assert encrypt_message("paz", 3) == ("zap")
